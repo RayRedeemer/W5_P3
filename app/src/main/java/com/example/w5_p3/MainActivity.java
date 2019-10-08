97718a2a2ac777b35a4b5ca4efd3997455af0f79
mirtim <mirtim@bu.edu>
2019-10-07 21:39:17 -0400

    upd

--- app/src/main/java/com/example/w5_p3/MainActivity.java
+++ app/src/main/java/com/example/w5_p3/MainActivity.java
@@ -3,6 +3,20 @@ package com.example.w5_p3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.net.*;
+import java.io.*;
 
 
 public class MainActivity extends AppCompatActivity implements game_layout.ControlFragmentListener {
@@ -12,7 +26,23 @@ public class MainActivity extends AppCompatActivity implements game_layout.Contr
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String dict = "";
+        try {
+            InputStream is = getAssets().open("words.txt");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            dict = new String(buffer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String lines[] = dict.split("\\r?\\n");
+        Toast.makeText(this, lines[0], Toast.LENGTH_LONG).show();
+
     }
+
 
     @Override
     public void sendScore(int score) {
